bc72d3cace64620390286f7f1a4abb9cfbb643de
pigs <potterpiggy13@outlook.com>
2022-10-26 22:19:28 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="App" i>
+      <h1 style={{color:"yellow"}}>Hobbees</h1>
+      <h2>(Its's a play on hobbies)</h2>
     </div>
   );
 }
